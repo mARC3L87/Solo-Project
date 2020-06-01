9e0ea189858772e9db7f57cec79206c0a66d87cd
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-06-01 21:51:50 +0200

    Add links and banners sections

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,28 @@ document.querySelector('.hamburger').addEventListener('click', function(e) {
   e.preventDefault();
   toggleMenu();
 });
+
+function togglePages(event){
+  event.preventDefault();
+  const clickedElement = this;
+  console.log(clickedElement);
+  //const activeLinks = document.querySelectorAll('.bar_links a');
+  //for(let activeLink of activeLinks){
+  //activeLink.classList.remove('active');
+  //console.log('activeLink', activeLink);
+  // }
+  //clickedElement.classList.add('active');
+  const activeContent = document.querySelectorAll('.content');
+  console.log(activeContent);
+  for(let content of activeContent){
+    content.classList.remove('active');
+  }
+  const contentSelector = clickedElement.getAttribute('href');
+  const targetContent = document.querySelector(contentSelector);
+  targetContent.classList.add('active');
+}
+const links = document.querySelectorAll('.bar_links a');
+for(let link of links){
+  link.addEventListener('click', togglePages);
+}
+
