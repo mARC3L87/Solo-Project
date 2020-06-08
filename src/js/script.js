7b8548c36870427d38f4368a9862b9d393da2956
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-06-08 23:05:22 +0200

    Add details section

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 'use strict';
 
 function toggleMenu(visible) {
@@ -75,4 +77,38 @@ document.querySelector('.exit_wrapper').addEventListener('click', function(e){
 document.getElementById('managerPhoto').addEventListener('click', function(e){
   e.preventDefault();
   openModal('#message-pop');
+});
+/* Chart*/
+const ctx = document.getElementById('myChart').getContext('2d');
+const chart = new Chart(ctx, {
+  // 1
+  type: 'bar',
+  data: {
+    // 2
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    // 3
+    datasets: [{
+      // 4
+      label: 'Signups',
+      // 5
+      backgroundColor: '#8DBEC8',
+      borderColor: '#8DBEC8',
+      // 6
+      data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+    },
+    {
+      label: 'FTD',
+      backgroundColor: '#F29E4E',
+      borderColor: '#F29E4E',
+      data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+    },
+    {
+      label: 'Earned',
+      backgroundColor: '#71B374',
+      borderColor: '#71B374',
+      data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+      // 7
+      hidden: true,
+    }]
+  },
 });
