1d30be3f959cdf228c5f8497e72795ab971e09dd
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-06-15 18:33:02 +0200

    Fix page change and responsive nav bar

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,52 +5,72 @@
 function toggleMenu(visible) {
   document.querySelector('.side_wrapper').classList.toggle('show', visible);
 }
-document.querySelector('.hamburger').addEventListener('click', function(e) {
-  e.preventDefault();
+document.querySelector('.hamburger').addEventListener('click', function(event) {
+  event.preventDefault();
   toggleMenu();
 });
+/* Activate page */
+const pages = document.getElementById('pages').children;
+const links = document.querySelectorAll('.bar_links li');
+const aLinks = document.querySelectorAll('.bar_links a');
+initPage();
 
-function togglePages(event){
-  event.preventDefault();
-  const clickedElement = this;
-  //const activeLinks = document.querySelectorAll('.bar_links a');
-  //for(let activeLink of activeLinks){
-  //activeLink.classList.remove('active');
-  //console.log('activeLink', activeLink);
-  // }
-  //clickedElement.classList.add('active');
-  const activeContent = document.querySelectorAll('.content');
-  console.log(activeContent);
-  for(let content of activeContent){
-    content.classList.remove('active');
+function initPage() {
+  const idFromHash = window.location.hash.replace('#/', '');
+
+  let pageMatchingHash = pages[0].id;
+
+  for (let page of pages) {
+    if (page.id == idFromHash) {
+      pageMatchingHash = page.id;
+      break;
+    }
   }
-  const contentSelector = clickedElement.getAttribute('href');
-  const targetContent = document.querySelector(contentSelector);
-  targetContent.classList.add('active');
+  activatePage(pageMatchingHash);
+
+  for (let link of links) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+      const clickedElement = event.target;
+      const id = clickedElement.getAttribute('href').replace('#', '');
+      activatePage(id);
+
+      window.location.hash = '#/' + id;
+    });
+  }
 }
-const links = document.querySelectorAll('.bar_links a');
-for(let link of links){
-  link.addEventListener('click', togglePages);
+
+function activatePage(pageId) {
+  for (let page of pages) {
+    if (page.id == pageId) {
+      page.classList.add('activePage');
+      page.classList.remove('in-active');
+    } else if (page.id != pageId) {
+      page.classList.remove('activePage');
+      page.classList.add('in-active');
+    }
+  }
+  toggleMenu(false);
 }
 /* Close Modal */
 function closeModal(){
   document.getElementById('overlay').classList.remove('show');
 }
 document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn){
-  btn.addEventListener('click', function(e){
-    e.preventDefault();
+  btn.addEventListener('click', function(event){
+    event.preventDefault();
     closeModal();
   });
 });
 
-document.querySelector('#overlay').addEventListener('click', function(e){
-  if(e.target === this){
+document.querySelector('#overlay').addEventListener('click', function(event){
+  if(event.target === this){
     closeModal();
   }
 });
 
-document.addEventListener('keyup', function(e){
-  if(e.keyCode === 27){
+document.addEventListener('keyup', function(event){
+  if(event.keyCode === 27){
     closeModal();
   }
 });
@@ -64,18 +84,26 @@ function openModal(modal){
 }
 /* Pop-up listeners */
 
-document.querySelector('.profile_wrapper').addEventListener('click', function(e){
-  e.preventDefault();
+document.querySelector('.profile_wrapper').addEventListener('click', function(event){
+  event.preventDefault();
+  openModal('#login-pop');
+});
+document.querySelector('.top-bar .icon-profile').addEventListener('click', function(event){
+  event.preventDefault();
   openModal('#login-pop');
 });
 
-document.querySelector('.exit_wrapper').addEventListener('click', function(e){
-  e.preventDefault();
+document.querySelector('.exit_wrapper').addEventListener('click', function(event){
+  event.preventDefault();
+  openModal('#quit-pop');
+});
+document.querySelector('.top-bar .icon-quit').addEventListener('click',function(event){
+  event.preventDefault();
   openModal('#quit-pop');
 });
 
-document.getElementById('managerPhoto').addEventListener('click', function(e){
-  e.preventDefault();
+document.getElementById('managerPhoto').addEventListener('click', function(event){
+  event.preventDefault();
   openModal('#message-pop');
 });
 /* Chart*/
